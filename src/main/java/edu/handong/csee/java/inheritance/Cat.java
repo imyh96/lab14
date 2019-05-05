5999d7155b916a73cba134cbe4da455860089df2
imyh96 <47966720+imyh96@users.noreply.github.com>
2019-05-05 17:54:39 +0900

    lab14 done?

---
+++ src/main/java/edu/handong/csee/java/inheritance/Cat.java
@@ -0,0 +1,35 @@
+package edu.handong.csee.java.inheritance;
+
+public class Cat extends Animal {
+	
+	String name = "Cat";
+	
+    public static void testClassMethod() {
+        System.out.println("The static method in Cat");
+    }
+    public void testInstanceMethod() {
+        System.out.println("The instance method in Cat");
+    }
+
+    public static void main(String[] args) {
+        Cat myCat = new Cat();
+        myCat.testClassMethod(); // hiding
+        myCat.testInstanceMethod(); // overriding
+        System.out.println(myCat.name); // hiding (not recommended)
+        
+        Animal myAnimal = myCat; // polymorphism
+        myAnimal.testClassMethod(); // hiding
+        myAnimal.testInstanceMethod(); // overriding
+        System.out.println(myAnimal.name); // hiding (not recommended)
+        
+        Animal myAnimal2 = new Dog(); // polymorphism
+        myAnimal2.testClassMethod(); // hiding
+        Dog myDog = (Dog) myAnimal2;
+        myDog.testClassMethod();
+        myAnimal2.testInstanceMethod(); // overriding
+        
+        
+    }
+}
+
+
---
+++ src/main/java/edu/handong/csee/java/inheritance/Dog.java
@@ -0,0 +1,12 @@
+package edu.handong.csee.java.inheritance;
+
+public class Dog extends Animal {
+
+	@Override
+	public void testInstanceMethod() {
+		// TODO Auto-generated method stub
+		//super.testInstanceMethod();
+		System.out.println("testInstamnceMethod in Dog");
+	}
+
+}
